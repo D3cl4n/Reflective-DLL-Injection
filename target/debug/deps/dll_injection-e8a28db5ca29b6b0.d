c4d370fd8c670146ffa3f778a5abab8eb37ce250
D3cl4n <dpmurphy@wpi.edu>
2023-08-25 00:17:54 -0400

    popup

---
+++ target/debug/deps/dll_injection-e8a28db5ca29b6b0.d
@@ -0,0 +1,7 @@
+C:\Users\D3cl4n\Reflective-DLL-Injection\target\debug\deps\dll_injection-e8a28db5ca29b6b0.exe: dll_injection\src\main.rs dll_injection\src\process.rs dll_injection\src\tests.rs
+
+C:\Users\D3cl4n\Reflective-DLL-Injection\target\debug\deps\dll_injection-e8a28db5ca29b6b0.d: dll_injection\src\main.rs dll_injection\src\process.rs dll_injection\src\tests.rs
+
+dll_injection\src\main.rs:
+dll_injection\src\process.rs:
+dll_injection\src\tests.rs:
--- target_dlls/src/lib.rs
+++ target_dlls/src/lib.rs
@@ -1,4 +1,14 @@
-#[no_mangle]
-pub extern "C" fn payload_entry() {
-    println!("Hello from payload entry!");
+extern crate winapi;
+
+use winapi::um::winnt::LPSTR;
+use winapi::um::winuser::{MessageBoxA, HWND_DESKTOP, MB_OK};
+
+#[no_mangle] //making sure the function name is not optimized or changed by compiler
+pub extern "C" fn payload_entry() { //specify extern C for C calling conventions ensuring compatibility with other langs
+    let msg = "Injected";
+    let caption = "DLL";
+    unsafe
+    {
+        MessageBoxA(HWND_DESKTOP, msg.as_ptr() as LPSTR, caption.as_ptr() as LPSTR, MB_OK);
+    }
 }
