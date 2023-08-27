79e46c899a61ea461c4b7016352db7391ea0d946
D3cl4n <dpmurphy@wpi..edu>
2023-08-26 23:13:34 -0400

    updated funbction

--- dll_injection/src/process.rs
+++ dll_injection/src/process.rs
@@ -2,16 +2,16 @@ extern crate winapi;
 extern crate sysinfo;
 
 use std::ptr;
-use sysinfo::SystemExt;
+use sysinfo::{System, SystemExt};
 use winapi::um::processthreadsapi::{OpenProcess};
 use winapi::um::winnt::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
 
-pub fn get_process_id_by_name(process_name: String)
+pub fn get_process_id_by_name(process_name: &String)
 {
     let mut system = sysinfo::System::new();
     system.refresh_all();
 
-    for process in system.get_process_by_name(process_name)
+    for process in system.processes_by_exact_name(&process_name)
     {
         println!("[+] Process name: {:?}", process.name);
         println!("[+] Nodepad PID: {:?}", process.pid);
