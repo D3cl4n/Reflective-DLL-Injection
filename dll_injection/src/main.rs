0c425484fea6cd3eb746701772c8a635feb88f2c
D3cl4n <dpmurphy@wpi.edu>
2023-08-29 00:37:49 -0400

    more stufgf

--- dll_injection/src/main.rs
+++ dll_injection/src/main.rs
@@ -4,13 +4,26 @@ mod tests;
 fn main() 
 {
     let process: &str = "notepad.exe";
-    crate::process::get_process_id_by_name(process);
+    let mut process_pid: u32 = 0;
+    match crate::process::get_process_id_by_name(process)
+    {
+        Some(pid) => process_pid = pid,
+        None => println!("[!] Failed to get PID from process name"),
+    }
+
     unsafe 
     {
-        match crate::process::open_process(6252)
+        match crate::process::open_process(process_pid)
         {
             Some(process_handle) => println!("[+] Opened process handle: {:?}", process_handle),
             None => println!("[!] Failed to open process handle"),
+        }
+
+        let dll_path: &str = "target\\debug\\target_dlls.dll";
+        match crate::process::get_dll_size(dll_path)
+        {
+            Some(length) => println!("yay"),
+            None => println!("no yay"),
         }
     }
 }
--- dll_injection/src/process.rs
+++ dll_injection/src/process.rs
@@ -2,20 +2,45 @@ extern crate winapi;
 extern crate sysinfo;
 
 use std::ptr;
-use sysinfo::{ProcessExt, System, SystemExt};
+use std::fs;
+use sysinfo::{ProcessExt, System, SystemExt, Pid, PidExt};
 use winapi::um::processthreadsapi::{OpenProcess};
 use winapi::um::winnt::{PROCESS_QUERY_INFORMATION, PROCESS_VM_READ};
 
-pub fn get_process_id_by_name(process_name: &str)
+pub fn get_dll_size(path: &str) -> Option<u64>
+{
+    println!("[+] Getting size of DLL: {:?}", path);
+    match fs::metadata(path)
+    {
+        Ok(metadata) => 
+        {
+            println!("[+] DLL length: {:?}", metadata.len());
+            return Some(metadata.len());
+        }
+
+        Err(e) =>
+        {
+            println!("[!] Error getting DLL length: {:?}", e);
+            return None;
+        }
+    }
+}
+
+pub fn get_process_id_by_name(process_name: &str) -> Option<u32>
 {
     println!("[+] Finding process PID for : {:?}", process_name);
     let s = System::new_all();
 
     for process in s.processes_by_exact_name(process_name)
     {
+        let process_id: Pid = process.pid();
         println!("[+] Process name: {:?}", process.name());
-        println!("[+] Nodepad PID: {:?}", process.pid());
+        println!("[+] Nodepad PID: {:?}", process_id.as_u32());
+
+        return Some(process_id.as_u32() as u32);
     }
+
+    None
 }
 
 pub unsafe fn open_process(process_id: u32) -> Option<u32>
